91d90ffda1f7715b47d42835767b83876a68975a
JonLz <jon.lazar@gmail.com>
2014-08-07 21:21:10 -0400

    Initial Commit

---
+++ IOS Challenge 1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IOS Challenge 1
+//
+//  Created by Jon on 8/7/14.
+//  Copyright (c) 2014 Reddit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ IOS Challenge 1Tests/IOS_Challenge_1Tests.m
@@ -0,0 +1,34 @@
+//
+//  IOS_Challenge_1Tests.m
+//  IOS Challenge 1Tests
+//
+//  Created by Jon on 8/7/14.
+//  Copyright (c) 2014 Reddit. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IOS_Challenge_1Tests : XCTestCase
+
+@end
+
+@implementation IOS_Challenge_1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
